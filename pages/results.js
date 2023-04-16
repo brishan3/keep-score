c52dfdc46b286965002d7234dc337c8f2dc8ae3a
Brishan King <brishan.king@gmail.com>
2023-04-16 12:41:55 -0700

    Fix build error

--- pages/results.js
+++ pages/results.js
@@ -3,7 +3,7 @@ import Results from '@/components/Results/Results';
 export default function Vote() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-24 px-4 md:px-12 bg-gradient-to-br from-blue-900 to-blue-700">
-      <h1 className='text-center text-gray-50 mb-4'>What's the Tally?</h1>
+      <h1 className='text-center text-gray-50 mb-4'>What&apos;s the Tally?</h1>
       <div className='bg-gray-600 max-w-xl w-full rounded-lg p-6'>
         <Results/>
       </div>
